3a7b00dd47274107e607a0134b3b371e87d64a18
BLUELOTUSCODING <jonahthomas20@yahoo.com>
2024-10-01 19:08:32 -0500

    more changes to, HinotamaBurstSpell, EpicFightClassesMod main class, and EFCWeaponCompabilityPresets.

--- src/main/java/net/bluelotuscoding/EFClasses/skill/weaponinnate/mage/HinotamaBurstSpell.java
+++ src/main/java/net/bluelotuscoding/EFClasses/skill/weaponinnate/mage/HinotamaBurstSpell.java
@@ -1,9 +1,21 @@
 package net.bluelotuscoding.EFClasses.skill.weaponinnate.mage;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import net.bluelotuscoding.EFClasses.gameasset.EFCAnimations;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import yesman.epicfight.api.animation.property.AnimationProperty;
+import yesman.epicfight.api.utils.math.ValueModifier;
 import yesman.epicfight.skill.Skill;
 import yesman.epicfight.skill.SkillContainer;
 import yesman.epicfight.skill.weaponinnate.WeaponInnateSkill;
@@ -13,6 +25,7 @@ import yesman.epicfight.world.capabilities.item.CapabilityItem;
 import yesman.epicfight.world.entity.eventlistener.PlayerEventListener;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public class HinotamaBurstSpell extends WeaponInnateSkill {
@@ -37,9 +50,32 @@ public class HinotamaBurstSpell extends WeaponInnateSkill {
             super.executeOnServer(executer, args);
     }
 
-    public List<Component> getTooltipOnItem(ItemStack itemStack, CapabilityItem cap, PlayerPatch<?> playerCap) {
-        List<Component> list = super.getTooltipOnItem(itemStack, cap, playerCap);
-        this.generateTooltipforPhase(list, itemStack, cap, playerCap, (Map)this.properties.get(0), "Each Strike:");
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public List<Component> getTooltipOnItem(ItemStack itemstack, CapabilityItem cap, PlayerPatch<?> playerpatch) {
+        List<Component> list = Lists.newArrayList();
+        List<Object> tooltipArgs = Lists.newArrayList();
+        String traslatableText = this.getTranslationKey();
+        double damage = playerpatch.getOriginal().getAttribute(Attributes.ATTACK_DAMAGE).getBaseValue() + EnchantmentHelper.getDamageBonus(itemstack, MobType.UNDEFINED);
+        ValueModifier damageModifier = ValueModifier.empty();
+
+        Set<AttributeModifier> damageModifiers = Sets.newHashSet();
+        damageModifiers.addAll(playerpatch.getOriginal().getAttribute(Attributes.ATTACK_DAMAGE).getModifiers());
+        damageModifiers.addAll(CapabilityItem.getAttributeModifiers(Attributes.ATTACK_DAMAGE, EquipmentSlot.MAINHAND, itemstack, playerpatch));
+
+        for (AttributeModifier modifier : damageModifiers) {
+            damage += modifier.getAmount();
+        }
+
+        this.getProperty(AnimationProperty.AttackPhaseProperty.DAMAGE_MODIFIER, this.properties.get(0)).ifPresent(damageModifier::merge);
+        damageModifier.merge(ValueModifier.multiplier(0.8F));
+        tooltipArgs.add(ChatFormatting.RED + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(damageModifier.getTotalValue((float)damage)));
+
+        list.add(Component.translatable(traslatableText).withStyle(ChatFormatting.WHITE).append(Component.literal(String.format("[%.0f]", this.consumption)).withStyle(ChatFormatting.AQUA)));
+        list.add(Component.translatable(traslatableText + ".tooltip", tooltipArgs.toArray(new Object[0])).withStyle(ChatFormatting.DARK_GRAY));
+
+        this.generateTooltipforPhase(list, itemstack, cap, playerpatch, this.properties.get(0), "Each Strike:");
+
         return list;
     }
 
